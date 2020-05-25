e09704d2b6ebf9a116478e3a2fff88077fffad08
Andrzej Popek <andrzejpopek93@gmail.com>
2020-05-25 11:32:09 +0200

    add header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import logomark from '../../../images/logomark.png';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Header.module.scss';
 
-const Component = ({ children }) => (
+const Component = () => (
   <div className={styles.root}>
-    <h2>Header</h2>
-    {children}
+    <div className={styles.logo}>
+      <img src={logomark} className={styles.logoMark} alt="saleswhale icon" />
+      <h1>saleswhale</h1>
+    </div>
+    <div className={styles.headers}>
+      <h2>Why Saleswhale?</h2>
+      <h2>Product</h2>
+      <h2>Company</h2>
+    </div>
+    <div className={styles.buttons}>
+      <button variant="contained" className={styles.button1}>
+        Log In
+      </button>
+      <button variant="contained" className={styles.button2}>
+        Get a demo
+      </button>
+    </div>
   </div>
 );
 
