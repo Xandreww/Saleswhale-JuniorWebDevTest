407df95d8f052ed6db3b7714b4b53a7bcf49f5c7
Andrzej Popek <andrzejpopek93@gmail.com>
2020-05-26 12:22:24 +0200

    add bottomFooter

---
+++ src/components/layout/Footer/BottomFooter/BottomFooter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import fb from '../../../../images/fb.png';
+import twitter from '../../../../images/twitter.png';
+import linkedin from '../../../../images/linkedin.png';
+import footerWaves from '../../../../images/footerWaves.png';
+
+import styles from './BottomFooter.module.scss';
+
+const Component = () => (
+  <div className={styles.root}>
+    <div className={styles.bottomFooter}>
+      <div className={styles.bottomFooterHeaders}>
+        <h1 className={styles.bottomFooterHeader}>Privacy</h1>
+        <h1 className={styles.bottomFooterHeader}>Terms</h1>
+      </div>
+      <div className={styles.bottomFooterIcons}>
+        <img src={fb} className={styles.footerIcon} alt="facebook icon" />
+        <img src={twitter} className={styles.footerIcon} alt="twitter icon" />
+        <img src={linkedin} className={styles.footerIcon} alt="linkedin icon" />
+      </div>
+    </div>
+    <p>© 2020 Saleswhale, Inc. All rights reserved.</p>
+    <img src={footerWaves} className={styles.footerWaves} alt="background waves" />
+  </div>
+);
+
+export {
+  Component as BottomFooter,
+  // Container as BottomFooter,
+  Component as BottomFooterComponent,
+};
--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { UpperFooter } from './UpperFooter/UpperFooter';
+import { BottomFooter } from './BottomFooter/BottomFooter';
 
 import styles from './Footer.module.scss';
 
 const Component = () => (
   <div className={styles.root}>
     <UpperFooter />
+    <BottomFooter />
   </div>
 );
 
